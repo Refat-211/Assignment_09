f6bec25a684d6487c46efd8b287759e5c219cf2d
Refat-211 <refathassan512@gmail.com>
2022-10-12 16:55:10 +0600

    sub header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
         </div>
         <div className='sub-header'>
           <img className='p-1 w-full h-80' src={quiztime} alt="" />
-          <p className='p-5 text-xl text-white'>The quiz maker that's easy to use, fun and fully customizable......</p>
+          <p className='p-5 text-xl text-white'>The quiz maker that's easy to use, fun and fully customizable.</p>
         </div>
       </div>
     );
