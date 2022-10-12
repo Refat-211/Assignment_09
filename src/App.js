229763cb94292b907a6caac005b9a398e5f543dc
Refat-211 <refathassan512@gmail.com>
2022-10-12 16:51:39 +0600

    navbar added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Blog from './components/Blog/Blog';
+import Error from './components/Error/Error';
+import Statistics from './components/Statistics/Statistics';
+import Topics from './components/Topics/Topics';
+import Main from './layouts/Main';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        { path: "/", element: <Topics></Topics> },
+        { path: "/topics", element: <Topics></Topics> },
+        { path: "/statistics", element: <Statistics></Statistics> },
+        { path: "/blog", element: <Blog></Blog> },
+      ],
+    },
+    { path: "*", element: <Error></Error> },
+  ]);
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Header.css'
+import { NavLink } from 'react-router-dom';
+import quiztime from '../../images/quiztimes.png'
+
+const Header = () => {
+    return (
+      <div>
+        <div className="header text-white lg:p-10 lg:flex-row sm:flex flex-col justify-between">
+          <h3 className="sm:text-xl font-bold lg:text-4xl">Quiz Maker</h3>
+          <nav className="lg:text-xl sm:text-lg">
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+              to="/topics"
+            >
+              Topics
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+              to="/statistics"
+            >
+              Statistics
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+          </nav>
+        </div>
+        <div className='sub-header'>
+          <img className='p-1 w-full h-80' src={quiztime} alt="" />
+          <p className='p-5 text-xl text-white'>The quiz maker that's easy to use, fun and fully customizable......</p>
+        </div>
+      </div>
+    );
+};
+
+export default Header;
